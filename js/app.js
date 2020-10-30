271611575e859f2080c7e18dfec8b52d021a1cd0
Saiteja Vemula <saitejavemula@gmail.com>
2020-10-30 15:47:37 -0700

    Initial Commit
    Uploaded all the files from the PC

---
+++ js/app.js
@@ -0,0 +1,3 @@
+// Key for the Basic Authorization in the form of btoa(clientID:clientSecret)
+let clientIDAndKey = "OGUwMzE3YjFhZmYzNGFmYzk0NjBlMDFjN2JkOGRmODY6OTA2MmVlZGJlYjVkNDg5NmFkZDllYmY0NzM2ZTYzYTQ=";
+
