4da48a320032fee6b6358c79c5241d9f755ad9bb
Saiteja Vemula <saitejavemula@gmail.com>
2020-11-08 10:11:01 +0530

    Refactored the code
    Added functions for code reusability.
    Added play/pause feature for sample music.

--- js/app.js
+++ js/app.js
@@ -1,131 +1,114 @@
-// Key for the Basic Authorization in the form of btoa(clientID:clientSecret)
-let clientIDAndKey =
+const clientIDAndKey = 
     "OGUwMzE3YjFhZmYzNGFmYzk0NjBlMDFjN2JkOGRmODY6OTA2MmVlZGJlYjVkNDg5NmFkZDllYmY0NzM2ZTYzYTQ=";
 let token = ``;
+let isTokenGenerated = false;
 
 // DOM ELEMENTS
-const form = document.querySelector("form");
-const content = document.querySelector(".content");
-const artistSearch = document.querySelector("#artistSearch");
-const artistImg = document.querySelector("#artist-img img");
-const artistName = document.querySelector("#artistName");
-const artistID = document.querySelector("#artistID");
-const artistGenres = document.querySelector("#artist-genres");
-const artistFollowers = document.querySelector("#followers");
+const form = document.querySelector('form');
+const artistImg = document.querySelector('#artist-img img');
+const artistName = document.querySelector('#artistName');
+const artistSearch = document.querySelector('#artistSearch');
+const artistID = document.querySelector('#artistID');
+const artistGenres = document.querySelector('#artist-genres');
+const artistFollowers = document.querySelector('#followers');
 const artistTracks = document.querySelector('#artist-tracks ul');
+const artistTracksContainer = document.querySelector('#artist-tracks');
+const audio = document.querySelector('audio');
 
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.close');
 
-artistSearch.focus();
+let currentSongs = [];
 
-// Event Listeners
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let artistName = artistSearch.value.trim().replaceAll(" ", "%20");
-    if (artistName) {
-        // console.log(artistName.toLowerCase());
-        // searchAnArtist(artistName);
-
-        // Fetch the Artist
-        fetch(
-            `https://api.spotify.com/v1/search?q=${artistName}&type=artist&limit=1&market=IN`,
-            {
-                method: "GET",
-                headers: {
-                    Authorization:
-                        "Bearer " + token,
-                },
-            }
-        )
-            .then((r) => r.json())
-            .then((data) => {
-                // Got the Artist's data
-                let d = data.artists.items[0];
-//                 console.log(`Artist Name : ${d.name}
-// External URL : ${d.external_urls.spotify}
-// No. of Followers: ${d.followers.total}
-// Spotify ID : ${d.id} 
-// Genres : ${d.genres}
-// Image : ${d.images[0].url}`
-                            // );
-                // Updating the UI
-                updateInfo( d.name, 
-                            d.external_urls.spotify, 
-                            d.images[0].url, 
-                            d.id, 
-                            d.genres,
-                            d.followers.total
-                );
-
-
-                // Fetching the artist's top tracks
-                fetch(
-                    `https://api.spotify.com/v1/artists/${d.id}/top-tracks?market=IN`,
-                    {
-                        method: "GET",
-                        headers: {
-                            Authorization:
-                                "Bearer " + token,
-                        },
-                    }
-                )
-                    .then((r) => r.json())
-                    .then((data) => {
-                        // console.log(data.tracks[0]);
-                        // console.log("Top 5 Tracks are : ");
-                        // data.tracks.filter((track, i) => {
-                        //     if(i<5) {
-                        //         console.log(`${track.name} ${calcTime(track.duration_ms)}`);
-                        //     }
-                        // })
-
-                        updateTracks(data.tracks.filter((t, i) => i < 5 ))
-                    }).catch(e => {
-                        console.log("Cannot Find the tracks : " + e.message);
-                    });
-            })
-            .catch((err) => {
-
-                updateInfo( 'Cannot find an artist', 
-                            '#artist-img', 
-                            '../img/404.jpg', 
-                            '-----',
-                            [],
-                            '');
-                updateTracks([])
-            });
-    }
-
-    form.reset();
-    form.blur();
-});
-
-// fetch() Functions
-async function getBearerToken() {
+// FETCH CALLS
+async function getToken() {
     let result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
+        method: 'POST',
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ${clientIDAndKey}`,
+            'Authorization': `Basic ${clientIDAndKey}`
         },
-        body: "grant_type=client_credentials",
+        body: "grant_type=client_credentials"
     });
 
-    // console.log(result);
-    if (result.status == 200) {
+    if(result.status === 200) {
         result = await result.json();
         token = result.access_token;
+
+        return true;
     } else {
-        console.log("Cannot Genereate the token");
+        console.log("Cannot generate a token. Error Code : " + result.status);
+    }
+
+    return false;
+}
+
+async function searchForArtist(name) {
+    name = name.trim().replaceAll(" ", "%20").toLowerCase();
+    let result = await fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1&market=IN`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    if(result.status === 200) {
+
+        result = await result.json();
+        if((result.artists.items.length)) {
+            result = result.artists.items[0];
+            artistTracksContainer.classList.remove('hide');
+            updateInfo(
+                    result.name,
+                    result.external_urls.spotify,
+                    result.images[0].url,
+                    result.id,
+                    result.genres,
+                    result.followers.total
+                ) ;
+
+            getTopTracks(result.id);
+        } else {
+            console.log("Cannot find the artist");
+            updateInfo(
+                "Cannot find the aritst",
+                "#",
+                "../img/404.jpg",
+                '123456789',
+                ['NOT FOUND'],
+                0
+            ) ;
+            artistTracksContainer.classList.add('hide');
+        }
+
+    } else {
+        console.log('Cannot search for the artist. Error Code : ' + result.status);
     }
 }
 
+async function getTopTracks(id) {
+    let result = await fetch(
+        `https://api.spotify.com/v1/artists/${id}/top-tracks?market=IN`,
+        {
+            method: "GET",
+            headers: {
+                Authorization:
+                    "Bearer " + token,
+            },
+        }
+    );
 
-getBearerToken().then(() => {
-    // console.log('Token = ' + token);
-    // searchAnArtist('The+Weeknd');
-});
+    if(result.status === 200) {
+        result = await result.json();
+        result = result.tracks.filter((d,i) => i<5);
+        // console.log("Before filter : \n",result);
+        updateTracks(result);
+    } else {
+        console.log("Cannot search for top tracks. Error code : " + result.status);
+    }
+}
 
-
+// NORMAL FUNCITONS
 function calcTime(ms) {
     let totalSecs = Math.round(ms / 1000);
     let mins = Math.floor(totalSecs / 60);
@@ -135,7 +118,11 @@ function calcTime(ms) {
 }
 
 function updateInfo(name, link, img, id, genres, followers) {
-    artistName.innerHTML = `<a href="${link}" target="_blank">${name}</a>`;
+    if(link === '#') {
+        artistName.innerHTML = `${name}`;
+    } else {
+        artistName.innerHTML = `<a href="${link}" target="_blank">${name}</a>`;
+    }
     artistID.textContent = `ID : ${id}`;
     artistImg.src = img;
     artistGenres.innerHTML = '';
@@ -146,18 +133,62 @@ function updateInfo(name, link, img, id, genres, followers) {
 }
 
 function updateTracks(tracks) {
-    console.log();
     artistTracks.innerHTML = '';
+    currentSongs = [];
     tracks.forEach((track, i) => {
+        currentSongs.push(track);
         artistTracks.innerHTML += `<li class="fadeFromLeft">
         <span class="sNo">${i+1}</span>
-        <span class="trackName">
-        <a href="${track.external_urls.spotify}" target="_blank">
-        ${track.name}
-        </a>
-        </span>
+        <span class="trackName">${track.name}</span>
         ${track.explicit ? '<span class="exp">E</span>' : '<span class="exp hide">E</span>'}
+        ${track.preview_url !== null ? 
+            `<span class="songURL" onclick="play(this, '${track.preview_url}')"><img src="./img/play.svg"></span>` :
+            '' 
+        }
         <span class="duration">${calcTime(track.duration_ms)}</span>
+        <span class="hide id">${track.id}</span>
     </li>`;
     });
 }
+
+function play(e, link) {
+    // console.log(e, e.children[0].src, link);
+    if( e.children[0].src.includes('play') ) {
+        audio.pause();
+        audio.src = link;
+        audio.play();
+        e.children[0].src = './img/pause.svg';
+    } else if( e.children[0].src.includes('pause') ) {
+        e.children[0].src = './img/play.svg';
+        audio.pause();
+    }
+}
+
+// Code starts here
+getToken().then(data => {
+    searchForArtist('The Weeknd');
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    searchForArtist(artistSearch.value);
+    form.reset();
+});
+
+artistTracks.addEventListener('click', (e) => {
+    
+    if(e.target.classList[0] === 'trackName') {
+        let title = popup.querySelector('h1');
+        title.textContent = e.target.innerText;
+        console.log(e.target.innerText);
+        popup.classList.remove('hide');
+    }
+
+});
+
+popup.addEventListener('click', e => {
+    if(e.target.className === 'close' || e.target.className === 'popup') {
+        popup.classList.add('hide');
+    }
+});
